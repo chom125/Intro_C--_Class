82c110bcde80b72c98950dd22350ee1e24b314e9
Steve Minor <stv.mnr@gmail.com>
2013-10-29 18:30:13 -0700

    finished homework 3

--- Assignment_3/Assignment_3/dateTest.cpp
+++ Assignment_3/Assignment_3/dateTest.cpp
@@ -16,26 +16,6 @@ struct Date
     int day;
 };
 
-static char daytab[2][13] = {
-	{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
-	{0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
-};
-
-std::string months[] = {
-	"January",
-	"Frebruary",
-	"March",
-	"April",
-	"May",
-	"June",
-	"July",
-	"August",
-	"September",
-	"October",
-	"November",
-	"December"
-};
-
 std::string days[] = {
 	"Sunday",
 	"Monday",
@@ -46,26 +26,25 @@ std::string days[] = {
 	"Saturday"
 };
 
-int getCenturyNumber(const Date& date){
-	// see centuries table:
-	// http://en.wikipedia.org/wiki/Determination_of_the_day_of_the_week
-	// basic method for mental calculation requires a century number
-	// 0-2-4-6 for 2000, 2100, 2200, 2300 (first two digits % 4)
-	return (date.year/100) % 4;
-}
-
-int getDayOfWeek(const Date& date){
-	// using basic method for mental calculation:
-	// http://en.wikipedia.org/wiki/Determination_of_the_day_of_the_week
-	// d + m + y + (y/4) + c;
-	// returns index for use with days[]
-	return date.day + date.month + date.year + (date.year/4) + ((date.year/100) % 4);
+int dow(const Date& date){
+	int y = date.year;
+	int m = date.month;
+	int d = date.day;
+	static int t[] = {0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4};
+	y -=m < 3;
+	return (y + y/4 - y/100 + y/400 + t[m-1] + d) % 7;
 }
 
 std::string getDayOfWeek(int dayIndex){
 	// get the day of the week by name
 	return days[dayIndex];
 }
+
+// required for following two functions
+static char daytab[2][13] = {
+	{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
+	{0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
+};
 
 /* day_of_year: set day of year from month & day */
 int day_of_year(int year, int month, int day)
@@ -102,7 +81,7 @@ void addDay(Date& date){
 void addMonth(Date& date){
 	// broad assumption that adding a month is 30 days
 	// could add a switch statement using dayTab and switch statement,
-	// but that would require 'adding a Februrary' which feels bizarre
+	// but that would require 'adding a Februrary' or similar which feels bizarre
 	addDays(date, 30);
 }
 
@@ -110,33 +89,98 @@ void addYear(Date& date){
 	date.year++;
 }
 
-Date getNextMonday(const Date&){
+Date getNextMonday(Date date){
 	// keep adding a day
 	// until the day equals monday
 	// then return that day
+	while (dow(date) != 1){
+		date.day++;
+	}
+	
+	return date;
 }
 
 void printDate(const Date& date){
-	std::cout << "\nday : " << date.day << std::endl;
-	std::cout << "month : " << date.month << std::endl;
+	//output the date in a readable format
+	std::cout << std::string(20, '*') << std::endl;
+	std::cout << "\nmonth : " << date.month << std::endl;
+	std::cout << "day : " << date.day << std::endl;
 	std::cout << "year : " << date.year << std::endl;
+	std::cout << "day of week : " << getDayOfWeek(dow(date)) << std::endl;
+	std::cout << std::string(20, '*') << std::endl;
 }
 
 
-
 TEST(dateTest, Date){
+	//validation based on http://www.timeanddate.com/date/weekday.html	
 	Date birthday;
 	birthday.day = 7;
 	birthday.month = 11;
 	birthday.year = 1984;
 	
-	addDay(birthday);
-	printDate(birthday);
-	std::cout << std::string('*', 20) << std::endl;
-	addMonth(birthday);
-	printDate(birthday);
-	std::cout << std::string('*', 20) << std::endl;
-	addYear(birthday);
-	printDate(birthday);
+	Date dday;
+	dday.day = 6;
+	dday.month = 6;
+	dday.year = 1944;
 	
+	Date apolloEleven;
+	apolloEleven.day = 20;
+	apolloEleven.month = 7;
+	apolloEleven.year = 1969;
+	
+	// begin addDay check	
+	addDay(birthday);
+	CHECK(birthday.day == 8 && birthday.month == 11 && birthday.year == 1984);
+	CHECK(getDayOfWeek(dow(birthday)) == "Thursday");
+	
+	addDay(dday);
+	CHECK(dday.day == 7 && dday.month == 6 && dday.year == 1944);
+	CHECK(getDayOfWeek(dow(dday)) == "Wednesday");
+	
+	addDay(apolloEleven);
+	CHECK(apolloEleven.day == 21 && apolloEleven.month == 7 && apolloEleven.year == 1969);
+	CHECK(getDayOfWeek(dow(apolloEleven)) == "Monday");	
+	// end addDay check
+	
+	// begin addMonth check
+	addMonth(birthday);
+	CHECK(birthday.day == 8 && birthday.month == 12 && birthday.year == 1984);
+	CHECK(getDayOfWeek(dow(birthday)) == "Saturday");
+	
+	addMonth(dday);
+	CHECK(dday.day == 7 && dday.month == 7 && dday.year == 1944);
+	CHECK(getDayOfWeek(dow(dday)) == "Friday");
+	
+	addMonth(apolloEleven);
+	CHECK(apolloEleven.day == 20 && apolloEleven.month == 8 && apolloEleven.year == 1969);
+	CHECK(getDayOfWeek(dow(apolloEleven)) == "Wednesday");
+	// end addMonth check
+	
+	// begin addYear check
+	addYear(birthday);
+	CHECK(birthday.day == 8 && birthday.month == 12 && birthday.year == 1985);
+	CHECK(getDayOfWeek(dow(birthday)) == "Sunday");
+	
+	addYear(dday);
+	CHECK(dday.day == 7 && dday.month == 7 && dday.year == 1945);
+	CHECK(getDayOfWeek(dow(dday)) == "Saturday");
+	
+	addYear(apolloEleven);
+	CHECK(apolloEleven.day == 20 && apolloEleven.month == 8 && apolloEleven.year == 1970);
+	CHECK(getDayOfWeek(dow(apolloEleven)) == "Thursday");
+	// end addYear check
+
+	// begin getNextMonday check
+	Date nmBirthday = getNextMonday(birthday);
+	CHECK(nmBirthday.day == 9 && nmBirthday.month == 12 && nmBirthday.year == 1985);
+	CHECK(getDayOfWeek(dow(nmBirthday)) == "Monday");
+	
+	Date nmDDay = getNextMonday(dday);
+	CHECK(nmDDay.day == 9 && nmDDay.month == 7 && nmDDay.year == 1945);
+	CHECK(getDayOfWeek(dow(nmDDay)) == "Monday");
+	
+	Date nmApolloEleven = getNextMonday(apolloEleven);
+	CHECK(nmApolloEleven.day == 24 && nmApolloEleven.month == 8 && nmApolloEleven.year == 1970);
+	CHECK(getDayOfWeek(dow(nmApolloEleven)) == "Monday");
+	// end getNextMonday check
 }
